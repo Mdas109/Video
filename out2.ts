b42079a466c7c18d3dc073d518edf934189f3e99
Anas <158191488+Mdas109@users.noreply.github.com>
2024-08-11 10:14:23 -0700

    Add files via upload

---
+++ out2.ts
@@ -0,0 +1,7 @@
+#EXTM3U
+#EXT-X-TARGETDURATION:10
+#EXT-X-VERSION:3
+#EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://tv.cloudcdn.me:80/live.ts?channelId=102479&uid=3842&deviceMac=00:1A:79:90:1D:C2
+#EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://tv.cloudcdn.me:80/live.ts?channelId=102479&uid=3842&deviceMac=00:1A:79:90:1D:C2s
